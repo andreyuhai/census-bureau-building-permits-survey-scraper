5f0805fc1c73bd97679bd64a2dc23b7d119c4132
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-07-22 14:59:45 +0200

    Scrape monthly method added

--- main.rb
+++ main.rb
@@ -9,8 +9,10 @@ building_permits_survey_scraper = BuildingPermitsSurveyScraper.new(
   scrape_dev_name: 'scrape_dev_name'
 )
 
-from_date = Date.new(2004, 01)
-to_date = Date.new(2019, 05)
+# from_date = Date.new(2004, 01)
+# to_date = Date.new(2019, 05)
+#
+# # Scrapes all the entries from 2004 - 04 (inclusive) to 2019 - 05 (inclusive)
+# building_permits_survey_scraper.scrape_between_dates(from_date, to_date)
 
-# Scrapes all the entries from 2004 - 04 (inclusive) to 2019 - 05 (inclusive)
-building_permits_survey_scraper.scrape_between_dates(from_date, to_date)
+building_permits_survey_scraper.scrape_previous_month
